a44adb66f08130d7e5e4260992074632a36008df
suhani <suhanipcte@gmail.com>
2026-04-15 15:59:53 +0530

    removed error

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -3,10 +3,10 @@ import { getNotes } from '../api/api'
 import Loader from '../components/Loader'
 import { logError } from '../utils/logger'
 
-function Notes() {
+function Notes () {
   const [notes, setNotes] = useState([])
   const [error, setError] = useState('')
-  const [loading, setLoading] = useState(true) // ✅ FIX
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getNotes()
@@ -18,27 +18,24 @@ function Notes() {
       .finally(() => setLoading(false))
   }, [])
 
-  if (loading) return <Loader />
-
-  if (error) {
-    return (
-      <div className='error'>
-        <p>{error}</p>
-        <button onClick={() => window.location.reload()}>
-          Retry
-        </button>
-      </div>
-    )
-  }
-
   return (
     <div className='notes-container'>
-         {/* <p>{undefined.name}</p>  */}
-      {notes.map(note => (
-        <div key={note._id} className='note-card'>
-          <p className='note-text'>{note.text}</p>
+      {error && (
+        <div className='error'>
+          <p>{error}</p>
+          <button onClick={() => window.location.reload()}>Retry</button>
         </div>
-      ))}
+      )}
+
+      {loading && <Loader />}
+
+      {!loading &&
+        !error &&
+        notes.map(note => (
+          <div key={note._id} className='note-card'>
+            <p className='note-text'>{note.text}</p>
+          </div>
+        ))}
     </div>
   )
 }
